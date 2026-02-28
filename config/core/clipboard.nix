198e13d19724b4be32eacf4d824ccb181d7d160a
Corbin Wunderlich <Corbin Wunderlich>
2026-02-27 21:42:34 -0500

    Add ctrl+c and ctrl+v

--- config/core/clipboard.nix
+++ config/core/clipboard.nix
@@ -2,4 +2,19 @@
   clipboard = {
     register = "unnamed";
   };
+
+  keymaps = [
+    {
+      key = "<C-c>";
+      mode = ["n" "v"];
+
+      action = "\"+y";
+    }
+    {
+      key = "<C-v>";
+      mode = ["n" "i"];
+
+      action = "\"+p";
+    }
+  ];
 }
